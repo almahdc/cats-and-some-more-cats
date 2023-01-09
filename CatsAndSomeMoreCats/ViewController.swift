72d6696476f4af5915e1ea761dd557cfbe6e1f77
Alma Hodzic <almahdc@gmail.com>
2023-01-09 17:03:24 +0100

    Do initial set up and clean up
    Remove Main, add folder structure, add test target, add initial README, gitignore

---
+++ CatsAndSomeMoreCats/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CatsAndSomeMoreCats
+//
+//  Created by Alma Hodzic on 09.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
