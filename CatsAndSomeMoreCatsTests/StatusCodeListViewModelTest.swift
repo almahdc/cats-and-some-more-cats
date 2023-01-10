f299dffb89defd4637b70f55f6da5087ce71ac04
Alma Hodzic <almahdc@gmail.com>
2023-01-10 14:47:03 +0100

    Add new tests

--- CatsAndSomeMoreCatsTests/StatusCodeListViewModelTest.swift
+++ CatsAndSomeMoreCatsTests/StatusCodeListViewModelTest.swift
@@ -24,39 +24,43 @@ final class StatusCodeListViewModelTest: XCTestCase {
         XCTAssertEqual(sut.sections.count, HTTPStatusCode.allCases.count)
     }
 
-    func testSectionOneLeftTitleEqualsHTTPStatusCodeSubscriptOneOnDidSelectSectionOne() {
-        // when
-        sut.didSelect(section: 1)
-
-        // then
-        XCTAssertEqual(sut.sections[1].leftTitle, "\(HTTPStatusCode.allCases[1].rawValue)")
+    func testSectionZeroWasNotTappedInitially() {
+        XCTAssertFalse(sut.sections[0].wasTapped.value)
     }
 
-    func testSectionOneWasTappedOnDidSelectSectionOne() {
+    func testSectionZeroLeftTitleEqualsHTTPStatusCodeSubscriptOneOnDidSelectSectionZero() {
         // when
-        sut.didSelect(section: 1)
+        sut.didSelect(section: 0)
 
         // then
-        XCTAssertTrue(sut.sections[1].wasTapped.value)
+        XCTAssertEqual(sut.sections[0].leftTitle, "\(HTTPStatusCode.allCases[0].rawValue)")
     }
 
-    func testSectionTwoWasNotTappedOnDidSelectSectionOne() {
+    func testSectionZeroWasTappedOnDidSelectSectionZero() {
+        // when
+        sut.didSelect(section: 0)
+
+        // then
+        XCTAssertTrue(sut.sections[0].wasTapped.value)
+    }
+
+    func testSectionZeroStaysTappedOnDidSelectSectionZeroMultipleTimes() {
         // given && when
         sut.showCatDetails = { _ in }
-        sut.didSelect(section: 1)
+        sut.didSelect(section: 0)
+        sut.didSelect(section: 0)
 
         // then
-        XCTAssertFalse(sut.sections[2].wasTapped.value)
+        XCTAssertTrue(sut.sections[0].wasTapped.value)
     }
 
-    func testSectionOneStaysTappedOnDidSelectSectionOneMultipleTimes() {
-        // given && when
-        sut.showCatDetails = { _ in }
-        sut.didSelect(section: 1)
-        sut.didSelect(section: 1)
+    func testTappedSectionGetsInsertedOnPositionZero() {
+        // when
+        sut.didSelect(section: 10)
 
         // then
-        XCTAssertTrue(sut.sections[1].wasTapped.value)
+        XCTAssertTrue(sut.sections[0].wasTapped.value)
+        XCTAssertFalse(sut.sections[10].wasTapped.value)
     }
 
     // MARK: – showCatDetails –
@@ -67,7 +71,7 @@ final class StatusCodeListViewModelTest: XCTestCase {
         sut.showCatDetails = { _ in count += 1 }
 
         // when
-        sut.didSelect(section: 1)
+        sut.didSelect(section: 0)
 
         // then
         XCTAssertEqual(count, 1)
