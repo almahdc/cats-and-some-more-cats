876794be11451a032d6a06fa61552475205825fa
Alma Hodzic <almahdc@gmail.com>
2023-01-10 12:32:46 +0100

    Add and remove some httpStatusCodes

--- CatsAndSomeMoreCats/API/HTTPStatusCodeList.swift
+++ CatsAndSomeMoreCats/API/HTTPStatusCodeList.swift
@@ -11,6 +11,7 @@ enum HTTPStatusCode: Int, CaseIterable {
     case `continue` = 100
     case switchingProtocols = 101
     case processing = 102
+    case earlyHints = 103
 
     case ok = 200
     case created = 201
@@ -20,8 +21,6 @@ enum HTTPStatusCode: Int, CaseIterable {
     case resetContent = 205
     case partialContent = 206
     case multiStatus = 207
-    case alreadyReported = 208
-    case imUsed = 226
 
     case multipleChoices = 300
     case movedPermanently = 301
@@ -29,7 +28,6 @@ enum HTTPStatusCode: Int, CaseIterable {
     case seeOther = 303
     case notModified = 304
     case useProxy = 305
-    case switchProxy = 306
     case temporaryRedirect = 307
     case permanentRedirect = 308
 
@@ -52,25 +50,36 @@ enum HTTPStatusCode: Int, CaseIterable {
     case rangeNotSatisfiable = 416
     case expectationFailed = 417
     case imATeapot = 418
+    case enhanceYourCalm = 420
     case misdirectedRequest = 421
     case unprocessableEntity = 422
     case locked = 423
     case failedDependency = 424
+    case tooEarly = 425
     case upgradeRequired = 426
-    case preconditionRequired = 428
     case tooManyRequests = 429
     case requestHeaderFieldsTooLarge = 431
+    case noResponse = 444
+    case blockedByWindowsParentalControls = 450
     case unavailableForLegalReasons = 451
+    case httpRequestSentToHTTPSPort = 497
+    case tokenExpired = 498
+    case clientClosedRequest = 499
 
     case internalServerError = 500
     case notImplemented = 501
     case badGateway = 502
     case serviceUnavailable = 503
     case gatewayTimeout = 504
-    case httpVersionNotSupported = 505
     case variantAlsoNegotiates = 506
     case insufficientStorage = 507
     case loopDetected = 508
+    case bandwidthLimitExeeded = 509
     case notExtended = 510
     case networkAuthenticationRequired = 511
+    case webServerIsDown = 521
+    case connectionTimedOut = 522
+    case originIsUnreachable = 523
+    case sslHandshakeFailed = 525
+    case networkConnectionTimeoutError = 599
 }
